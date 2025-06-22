8698199157056e869d209d900338dcca7b416b64
Petr Janda <jape@jape.tech>
2025-06-22 14:15:25 +0200

    Added new party capability with validators

--- party_app/routes/main.py
+++ party_app/routes/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from party_app.routes import party_list, party_detail
+from party_app.routes import party_list, party_detail, new_party
 
 api_router = APIRouter()
 api_router.include_router(party_list.router)
 api_router.include_router(party_detail.router)
+api_router.include_router(new_party.router)
